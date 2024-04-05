d2106578c932d5cda99f5cf719ba505cab57c729
Landon Williams <42020212+landwill@users.noreply.github.com>
2024-04-05 22:16:05 +1000

    fix: ALL blank titles appear as 'Untitled'

--- src/components/DocumentSelectorList.tsx
+++ src/components/DocumentSelectorList.tsx
@@ -22,7 +22,7 @@ export const DocumentSelectorList = observer(() => {
       documentIdentifiers.slice().sort((a, b) => b.lastModified - a.lastModified).map(document => {
         return <DocumentSelectorItem key={document.documentUuid}
                                      isActive={selectedDocumentUuid === document.documentUuid}
-                                     filename={document.documentTitle === '' ? 'Untitled' : document.documentTitle}
+                                     filename={!document.documentTitle.trim() ? 'Untitled' : document.documentTitle}
                                      onClick={() => {runInAction(() => {documentStore.selectedDocumentUuid = document.documentUuid})}} />
       })
     }

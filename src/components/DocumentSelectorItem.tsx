86428d19edfa2a8b18c081184d3ceeff359d436a
Landon Williams <42020212+landwill@users.noreply.github.com>
2024-04-05 20:48:29 +1000

    style: document selector items indicate when hovered

--- src/components/DocumentSelectorItem.tsx
+++ src/components/DocumentSelectorItem.tsx
@@ -7,7 +7,8 @@ interface DocumentSelectorItemProps {
 }
 
 export function DocumentSelectorItem({ isActive, filename, onClick }: DocumentSelectorItemProps) {
-  const className = isActive ? 'active' : undefined
+  let className = 'document-selector-item'
+  if (isActive) className += ' active'
 
   return <span className={className}
                style={{ marginBottom: '0.25em', textOverflow: 'ellipsis', textWrap: 'nowrap', overflow: 'hidden' }}

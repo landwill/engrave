f14defedd3b97d106fff35ad19286f77ca444bfd
Landon Williams <42020212+landwill@users.noreply.github.com>
2024-04-26 10:31:06 +1000

    filepicker: major refactoring of movement logic

--- src/components/filepicker/utils.ts
+++ src/components/filepicker/utils.ts
@@ -28,46 +28,82 @@ function searchTreeForContainingList(items: FileTreeItem[], itemUuid: string): {
   return null
 }
 
-export const moveElementToFolder = (fileTree: FileTreeItem[], sourceUuid: string, targetFolderUuid: string | undefined, isFolder: boolean) => {
-  if (sourceUuid === targetFolderUuid) return
-
-  let targetChildren
-  if (targetFolderUuid === undefined) {
-    targetChildren = fileTree
+function addForeignElementToFileTree(targetBranch: FileTreeFolder, sourceUuid: string, sourceIsFolder: boolean) {
+  let newFileTreeEntry: FileTreeItem
+  if (sourceIsFolder) {
+    newFileTreeEntry = { uuid: sourceUuid, isFolder: true, children: [] }
   } else {
-    const targetBranch = searchTreeForFolder(fileTree, targetFolderUuid)
-    if (targetBranch == null) {
-      console.error('Target branch not found when moving element to folder.')
+    newFileTreeEntry = { uuid: sourceUuid, isFolder: false }
+  }
+  targetBranch.children.push(newFileTreeEntry)
+}
+
+const performMove = (targetList: FileTreeItem[], source: {
+  item: FileTreeItem;
+  parent: FileTreeItem[];
+  index: number
+}) => {
+  if (targetList === source.parent) return
+
+  targetList.push(source.item)
+  source.parent.splice(source.index, 1)
+}
+
+function moveElementFromOneFolderToAnother(sourceContainingList: {
+  item: FileTreeItem;
+  parent: FileTreeItem[];
+  index: number
+}, targetBranch: FileTreeFolder, targetUuid: string) {
+  // item found in fileTree; move its corresponding info (isFolder, children, etc.) from the source
+  const { item } = sourceContainingList
+
+  // Quit early if we're dragging a folder into one of its child (even nested) folders
+  if (
+    item.isFolder // this risk only exists for folders
+    && searchTreeForContainingList(item.children, targetUuid) // i.e. the target was found in the
+  ) {
+    console.debug('Cannot move folders into their own subfolders (even if nested).')
+    return
+  }
+
+  performMove(targetBranch.children, sourceContainingList)
+}
+
+function moveFolderToTopLevel(fileTree: FileTreeItem[], sourceUuid: string) {
+  const sourceContainingList = searchTreeForContainingList(fileTree, sourceUuid)
+  if (sourceContainingList == null) {
+    console.error('moveFolderToTopLevel found a null sourceContainingList. What causes this?', sourceUuid)
+    return
+  }
+  performMove(fileTree, sourceContainingList)
+}
+
+export const moveElementToFolder = (fileTree: FileTreeItem[], sourceUuid: string, targetUuid: string | undefined, sourceIsFolder: boolean) => {
+  if (sourceUuid === targetUuid) return
+
+  if (targetUuid === undefined) {
+    if (sourceIsFolder) {
+      moveFolderToTopLevel(fileTree, sourceUuid)
+      return
+    } else {
+      // removeElementFromFileTree(sourceUuid) // todo implement
       return
     }
-    targetChildren = targetBranch.children
+  }
+
+  const targetBranch = searchTreeForFolder(fileTree, targetUuid)
+  if (targetBranch == null) {
+    console.error('Target branch not found when moving element to folder. Unknown scenario. Source: {}. Target: {}.', sourceUuid, targetUuid)
+    return
   }
 
   const sourceContainingList = searchTreeForContainingList(fileTree, sourceUuid)
 
-  if (sourceContainingList != null) {
-    // item found in fileTree; move its corresponding info (isFolder, children, etc.) from the source
-    const { item, parent, index } = sourceContainingList
-
-    // Quit early if we're dragging a folder into one of its child (even nested) folders
-    if (targetFolderUuid != null // If null, we're dragging to the top-level list, which doesn't contain this risk
-      && item.isFolder // this risk only exists for folders
-      && searchTreeForContainingList(item.children, targetFolderUuid) // i.e. the target was found in the
-    ) {
-      console.debug('Cannot move folders into their own subfolders (even if nested).')
-      return
-    }
-
-    targetChildren.push(item)
-    parent.splice(index, 1)
+  if (sourceContainingList == null) {
+    addForeignElementToFileTree(targetBranch, sourceUuid, sourceIsFolder)
+    return
   } else {
-    // item not found in fileTree; it's presumably stored outside the fileTree, and is being newly added
-    let newFileTreeEntry
-    if (isFolder) {
-      newFileTreeEntry = { uuid: sourceUuid, isFolder, children: [] }
-    } else {
-      newFileTreeEntry = { uuid: sourceUuid, isFolder }
-    }
-    targetChildren.push(newFileTreeEntry)
+    moveElementFromOneFolderToAnother(sourceContainingList, targetBranch, targetUuid)
+    return
   }
 }

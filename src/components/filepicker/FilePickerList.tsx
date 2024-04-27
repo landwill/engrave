bb0296a0edaed2f09e4b0a2a57ad09e6d06ec1e7
Landon Williams <42020212+landwill@users.noreply.github.com>
2024-04-27 11:16:45 +1000

    filepicker: fix #27

--- src/components/filepicker/FilePickerList.tsx
+++ src/components/filepicker/FilePickerList.tsx
@@ -1,8 +1,9 @@
-import { monitorForElements } from '@atlaskit/pragmatic-drag-and-drop/element/adapter'
+import { combine } from '@atlaskit/pragmatic-drag-and-drop/combine'
+import { dropTargetForElements, monitorForElements } from '@atlaskit/pragmatic-drag-and-drop/element/adapter'
 import { DropTargetRecord } from '@atlaskit/pragmatic-drag-and-drop/types'
 import { action } from 'mobx'
 import { observer } from 'mobx-react-lite'
-import { CSSProperties, useEffect } from 'react'
+import { CSSProperties, useEffect, useRef } from 'react'
 import { DraggableSource, DropTargetLocation, FileTreeItem } from '../../interfaces.ts'
 import { documentStore } from '../../stores/DocumentStore.ts'
 import { fileTreeStore } from '../../stores/FileTreeStore.ts'
@@ -32,25 +33,30 @@ const flattenFileTreeUuids = (fileTree: FileTreeItem[]) => {
 
 export const FilePickerList = observer(() => {
   const fileTreeUuids = flattenFileTreeUuids(fileTreeStore.fileTreeData)
+  const ref = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
-    return monitorForElements({
-      onDrop: action(({ source, location }) => {
-        const sourceData = source.data.source as DraggableSource | null | undefined // unsure if null or defined, so declaring both
-        if (sourceData == null) return
-        const { uuid, isFolder: sourceIsFolder } = sourceData
-        const destination: DropTargetRecord | undefined = location.current.dropTargets[0]
+    const element = ref.current
+    if (element == null) throw new Error('null FilePickerList ref.current')
 
-        // suppressed because despite Pragmatic's type-hinting, null/undefined is possible
-        // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-        if (destination == null) return
-        const destinationLocation = destination.data.location as DropTargetLocation
-        moveElementToFolder(fileTreeStore.fileTreeData, uuid, destinationLocation.uuid, sourceIsFolder)
-      })
-    })
+    return combine(monitorForElements({
+        onDrop: action(({ source, location }) => {
+          const sourceData = source.data.source as DraggableSource | null | undefined // unsure if null or defined, so declaring both
+          if (sourceData == null) return
+          const { uuid, isFolder: sourceIsFolder } = sourceData
+          const destination: DropTargetRecord | undefined = location.current.dropTargets[0]
+
+          // suppressed because despite Pragmatic's type-hinting, null/undefined is possible
+          // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+          if (destination == null) return
+          const destinationLocation = destination.data.location as DropTargetLocation
+          moveElementToFolder(fileTreeStore.fileTreeData, uuid, destinationLocation.uuid, sourceIsFolder)
+        })
+      }),
+      dropTargetForElements({ element, getData: () => ({ location: { uuid: undefined } as DropTargetLocation })}))
   }, [])
 
-  return <div style={DIV_STYLE} id='file-picker-list'>
+  return <div style={DIV_STYLE} id='file-picker-list' ref={ref}>
     {
       fileTreeStore.fileTreeData.map(item => <FileTreeComponent key={item.uuid} item={item} />)
     }
